012c344ef9bccf491d63f7da3bc35092e4c04153
Ning Yi <ialeonard@ucdavis.edu>
2019-08-19 12:48:28 -0700

    sprint challenge submission

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,6 +1,6 @@
 import hashlib
 import requests
-
+import json
 import sys
 
 from uuid import uuid4
@@ -19,11 +19,52 @@ def proof_of_work(last_proof):
     - p is the previous proof, and p' is the new proof
     """
 
+
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    proof = 419824       
     #  TODO: Your code here
+
+    iterator = 0
+    found_proof = False
+    last_proof_string = json.dumps(last_proof, sort_keys=True).encode()
+
+    last_hash = hashlib.sha256(last_proof_string).hexdigest()
+    #last_hash = 'bum'
+    #last_hash = m.update(b'last_proof')
+    #print(m)
+    print("last proof and last hash : ", last_proof, last_hash)
+    # 343111193
+    # 64951984
+    string1 = json.dumps(419824, sort_keys=True).encode()
+    hash1 = hashlib.sha256(string1).hexdigest()
+    string2 = json.dumps(6546077, sort_keys=True).encode()
+    hash2 = hashlib.sha256(string2).hexdigest()
+    print("RESULT ", hash1, hash2)
+    while found_proof is False:
+        iterator += 1
+        if iterator % 100000 == 0:
+            iterator = 0
+            r = requests.get(url=node + "/last_proof")
+            data = r.json()
+            if data.get('proof') != last_proof:
+                proof = last_proof - 1
+                print("our proof is now ", proof)
+                last_proof = data.get('proof')
+                print("Block updated on server. New proof is ", last_proof, type(last_proof))
+                #proof = 70000000
+                last_proof_string = json.dumps(last_proof, sort_keys=True).encode()
+
+                last_hash = hashlib.sha256(last_proof_string).hexdigest()
+
+        if valid_proof(last_hash, proof) == True:
+            print("Got a valid_proof")
+            found_proof = True
+            break 
+        # if proof % 1000000 == 1:
+        #     print('current proof is ', proof)
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,8 +79,20 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    # hash new proof
+    proof_string = json.dumps(proof, sort_keys=True).encode()
+    new_hash = hashlib.sha256(proof_string).hexdigest()
 
+    # get ahold of the last six digits of the last hash
+    # if proof % 1000000 == 1:
+    #     print("first six digits of hash ", new_hash[:6], "with proof = ", proof, "last six of prev hash " , last_hash[-6:])
+    # get ahold of the first six digits of the new hash
+
+    # compare the last two
+    if new_hash[:6] == last_hash[-6:]:
+        return True
+    
+    return False
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -77,5 +130,6 @@ if __name__ == '__main__':
         if data.get('message') == 'New Block Forged':
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
+            
         else:
             print(data.get('message'))
--- written_responses.md
+++ written_responses.md
@@ -2,6 +2,13 @@
 
 1. Describe the general process of how blocks are added to a blockchain.
 
+	For a new block to be added to the block chain it must have the hash of the previous blocks in the chain and must complete the proof of work required for the particular blockchain it is a part of. The proof of work is basically a time consuming computation which is designed to slow down the creation of new blocks for the purpose of security. If a block somewhere in the chain is tampered with, its hash changes and all the subsequent blocks end up becoming invalid since they contain the original hash of the tampered block. So someone who wants to change a block will have to quickly modify the blocks that come after the tampered block. The time required for the proof of work makes it extremely unlikely that any one person can do all of the block calculations necessary before other people have had time to add one new block. The blockchain is also not stored on a central server but on a P2P network with many copies of the original chain. If a new block is created that is consistent with the chain and matches the copy the majority of people in the network have it can be added to the chain.
+
 2. How can blockchain users mine coins?
 
+	Blockchain users mine coins by solving the proof of work required for a blocks creation. As mentioned above, this is a complex calculation that takes time and energy. Once the proof of work is solved the new block which contains the transaction information can be validated. So the transaction can be securely completed. The miners who facilitate this are rewarded with either bitcoins or part of the transaction fees.
+
+
 3. Explain how simulations like Conway's Game of Life can be used in real-world applications.
+
+	Conway's Game of Life is based on a few rules which determine the next state or generation of the system. Many real-world scenarios also represent a system that changes over time based on simple mathematical, physical, chemical, or biological rules. Examples of these are the growth rate of bacteria, modeling the life cycle of yeast, modeling the way a projectile descends based on the laws of physics, how much heat will be left in a star over time. There are many more. By simulating the system we can determine what will happen to it, and when it will happen before it actually happens. Another way of saying this is that we can predict the future accurately and this is obviously a very powerful thing.
